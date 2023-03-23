a4be4a43a0185918fb22f1e475004237414f178b
Heveliev <109628656+Heveliev@users.noreply.github.com>
2023-03-23 21:18:15 +0000

    Deploying to gh-pages from @ Heveliev/product-store@0344816ec0fdc00bac61743091694a3936381852 🚀

---
+++ static/js/228.7be3b372.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[228],{9228:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("div",{children:"AddNewProduct"})})}}}]);
+//# sourceMappingURL=228.7be3b372.chunk.js.map
